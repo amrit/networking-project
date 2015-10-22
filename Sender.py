55218084a1a72141b4130840dbce8a5e104a6cff
Amrit Ayalur <a.ayalur@gmail.com>
2015-10-21 22:39:58 -0700

    Pass test 2 lol sweet

--- Sender.py
+++ Sender.py
@@ -17,41 +17,96 @@ class Sender(BasicSender.BasicSender):
         self.sequence_number = 0
         self.complete = False
         self.next_seq = 0
+        self.ack_received_table = {}
+        self.window_size = 7
+        self.window = []
+        self.last_packet_seqno = -1
+
+
+
+    # Function to read data in from the file.
+    def get_data(self):
+        data = self.infile.read(1400)
+        return data
+
+    def send_packet(self):
+        if self.sequence_number == 0:
+            msg = "syn"
+            pack = self.make_packet(msg, self.sequence_number, "")
+            self.send(pack)
+        else:
+            # Else if you get data and it's empty - that means the file has been fully sent. Send a fin message to end transmission.
+            #while len(self.window) <= self.window_size:
+            for seqno in range(self.sequence_number, self.sequence_number+7):
+                data = self.get_data()
+                if data == "":
+                    msg = "fin"
+                   # self.complete = True
+                    self.last_packet_seqno = seqno
+                    pack = self.make_packet(msg, seqno, "")
+                    self.window.append(pack)
+                    self.send(pack)
+                    break
+                else:
+                    msg = "dat"
+                    pack = self.make_packet(msg, seqno, data)
+                    self.send(pack)
+                    #self.window.append(pack)
+
+            # for pack in self.window:
+            #     print pack
+
+            #     self.send(pack)
+            #     self.window.remove(pack)
+
+    def process_response(self, response):
+        msg_type, seqno, reponse_data, checksum = self.split_packet(response)
+
+        # Add the ACK to ack_received_table
+        if msg_type == "ack":
+            if seqno in self.ack_received_table:
+                self.ack_received_table[seqno] += 1
+            else:
+                self.ack_received_table[seqno] = 1
+
+        # If the checksum is valid
+        if Checksum.validate_checksum(response):
+            
+            # Fast retransmit
+            if self.ack_received_table[seqno] == 4:
+                self.sequence_number = int(seqno)
+
+            # If the ack is expecting the next seq number, send it
+            elif int(seqno) == self.sequence_number + 1:
+                self.sequence_number += 1
+
+            else:
+                # A packet was dropped, set our sequence number to the one the receiver expects.
+                self.sequence_number = int(seqno)
+
+            self.send_packet()
+
 
 
     # Main sending loop.
     def start(self):
-        while not self.complete:
-            if self.sequence_number == 0:
-                msg = "syn"
-                pack = self.make_packet(msg, self.sequence_number, "")
-                self.send(pack)
-            else:
-                data = self.get_data()
-                if data == "":
-                    msg = "fin"
-                    self.complete = True
-                else:
-                    msg = "dat"
 
-                pack = self.make_packet(msg, self.sequence_number, data)
-                self.send(pack)
-
+        # While the whole file has not been sent.
+        while not self.complete: 
+            if self.last_packet_seqno != -1:
+                self.complete = True           
+            # Wait for ACK from receiver
             response = self.receive(.5)
+            if response is not None:
+                self.process_response(response)
+            elif self.sequence_number == 0:
+                self.send_packet()
 
-            msg_type, seqno, reponse_data, checksum = self.split_packet(response)
 
-            if Checksum.validate_checksum(response):
-                if int(seqno) == self.sequence_number + 1:
-                    self.sequence_number += 1
-                else:
-                    self.sequence_number = int(seqno)
+
             
 
 
-    def get_data(self):
-        data = self.infile.read(1400)
-        return data
 
 
         
